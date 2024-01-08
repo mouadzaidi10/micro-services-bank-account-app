51c7cb4a14ac0386499da964108878ca8d01aed9
mouad zaidi <mouad.zaidi@etu.uae.ac.ma>
2024-01-08 11:47:31 +0100

    Add 4 services: account-service, config-service, discovery-service, gateway-service

---
+++ gateway-service/src/main/java/net/zaidi/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package net.zaidi.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+
+}
